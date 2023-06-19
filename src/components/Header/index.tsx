661c3dfd6474d2136c9ffe45342e4ab62b4335f4
5pamm3r <cmassarino.dev@gmail.com>
2023-06-19 18:24:03 -0300

    Add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import '../../styles/layout/_header.scss'
+import React from "react";
+import "../../styles/layout/_header.scss";
 
 const Header: React.FC = () => {
   return (
-    <header className='header__container'>
+    <header className="header__container">
       <nav>
-        <div><button>1</button></div>
-        <div><button>2</button></div>
-        <div><button>3</button></div>
-        <div><button>4</button></div>
-
+        <div className="headerItem__container">
+          <button>1</button>
+          <div className="headerInfo__container">
+            <span>step 1</span>
+            <span>your info</span>
+          </div>
+        </div>
+        <div className="headerItem__container">
+          <button>2</button>
+          <div className="headerInfo__container">
+            <span>step 2</span>
+            <span>select plan</span>
+          </div>
+        </div>
+        <div className="headerItem__container">
+          <button>3</button>
+          <div className="headerInfo__container">
+            <span>step 3</span>
+            <span>add-ons</span>
+          </div>
+        </div>
+        <div className="headerItem__container">
+          <button>4</button>
+          <div className="headerInfo__container">
+            <span>step 4</span>
+            <span>summary</span>
+          </div>
+        </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
