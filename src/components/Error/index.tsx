f2007813338f11d5a2da4cd9d163cc1a013ced56
5pamm3r <cmassarino.dev@gmail.com>
2023-06-19 18:23:15 -0300

    Add error component

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error: React.FC = () => {
+  return (
+    <p style={{ color: 'red', display: 'inline-block', position: 'absolute', right: 0 }}>This field is required.</p>
+  )
+}
+
+export default Error
