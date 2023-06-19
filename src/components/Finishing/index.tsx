7ed56dac01a02646d8dde2f50a8112f91696dbf0
5pamm3r <cmassarino.dev@gmail.com>
2023-06-19 18:23:35 -0300

    Change class

--- src/components/Finishing/index.tsx
+++ src/components/Finishing/index.tsx
@@ -26,32 +26,32 @@ const Finishing: React.FC = () => {
     <div className='finishing__container'>
       <h2 className='title'>Finishing up</h2>
       <h3 className='subtitle'>Double-check everything looks OK before confirming.</h3>
-      <div className='plans__container'>
-        <div className='plan__container'>
+      <div className='detail__container'>
+        <div className='detailPlan__container'>
           <div className='planName__container'>
-            <span>{planSelected.name} ({planSelected.plan})</span>
+            <span className='planName'>{planSelected.name} ({planSelected.plan})</span>
             <a href='#'>Change</a>
           </div>
           <div className='planPrice__container'>
-            <span>${planSelected.price}/{planSelected.plan === 'monthly' ? 'mo' : 'yr'}</span>
+            <span className='planPrice'>${planSelected.price}/{planSelected.plan === 'monthly' ? 'mo' : 'yr'}</span>
           </div>
         </div>
         <hr />
-        <div className='addOns__container'>
+        <div className='detailAddOns__container'>
           {addOns.map(e => {
             if (e.state === true) {
               return (
-                <div key={e.id}>
-                  <span>{e.name}</span>
-                  <span>+${e.price}/{e.plan}</span>
+                <div key={e.id} className='detailAddOns'>
+                  <span className='detailAddOns-name'>{e.name}</span>
+                  <span className='detailAddOns-price'>+${e.price}/{e.plan}</span>
                 </div>)
             }
           })}
         </div>
-        <div className='total__container'>
-          <span>Total (per {planSelected.plan}) </span>
-          <span>+${total}/{planSelected.plan}</span>
-        </div>
+      </div>
+      <div className='total__container'>
+        <span className='total'>Total (per {planSelected.plan}) </span>
+        <span className='total-price'>+${total}/{planSelected.plan === 'monthly' ? 'mo' : 'yr'}</span>
       </div>
     </div>
   )
