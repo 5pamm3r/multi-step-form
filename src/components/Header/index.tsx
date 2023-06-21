7f5ec630e85e5aac573f5d9fa9ad631029a99c92
5pamm3r <cmassarino.dev@gmail.com>
2023-06-21 20:24:26 -0300

    Add new className

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,36 @@
 import React from "react";
 import "../../styles/layout/_header.scss";
 
-const Header: React.FC = () => {
+interface Props {
+  step: number;
+}
+const Header: React.FC<Props> = ({ step }) => {
   return (
     <header className="header__container">
       <nav>
         <div className="headerItem__container">
-          <button>1</button>
+          <button className={step === 1 ? 'paint' : ''}>1</button>
           <div className="headerInfo__container">
             <span>step 1</span>
             <span>your info</span>
           </div>
         </div>
         <div className="headerItem__container">
-          <button>2</button>
+          <button className={step === 2 ? 'paint' : ''}>2</button>
           <div className="headerInfo__container">
             <span>step 2</span>
             <span>select plan</span>
           </div>
         </div>
         <div className="headerItem__container">
-          <button>3</button>
+          <button className={step === 3 ? 'paint' : ''}>3</button>
           <div className="headerInfo__container">
             <span>step 3</span>
             <span>add-ons</span>
           </div>
         </div>
         <div className="headerItem__container">
-          <button>4</button>
+          <button className={step > 3 ? 'paint' : ''}>4</button>
           <div className="headerInfo__container">
             <span>step 4</span>
             <span>summary</span>
