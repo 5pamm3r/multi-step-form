1a8517a7f732c7072f15a4b2ac1d6b6f22229b27
5pamm3r <cmassarino.dev@gmail.com>
2023-06-22 11:04:18 -0300

    Add setStep

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,34 +3,37 @@ import "../../styles/layout/_header.scss";
 
 interface Props {
   step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
 }
-const Header: React.FC<Props> = ({ step }) => {
+
+//agregar validaciones para setStep.
+const Header: React.FC<Props> = ({ step, setStep }) => {
   return (
     <header className="header__container">
       <nav>
         <div className="headerItem__container">
-          <button className={step === 1 ? 'paint' : ''}>1</button>
+          <button className={step === 1 ? 'paint' : ''} onClick={() => setStep(1)}><span>1</span></button>
           <div className="headerInfo__container">
             <span>step 1</span>
             <span>your info</span>
           </div>
         </div>
         <div className="headerItem__container">
-          <button className={step === 2 ? 'paint' : ''}>2</button>
+          <button className={step === 2 ? 'paint' : ''} onClick={() => setStep(2)}><span>2</span></button>
           <div className="headerInfo__container">
             <span>step 2</span>
             <span>select plan</span>
           </div>
         </div>
         <div className="headerItem__container">
-          <button className={step === 3 ? 'paint' : ''}>3</button>
+          <button className={step === 3 ? 'paint' : ''} onClick={() => setStep(3)}><span>3</span></button>
           <div className="headerInfo__container">
             <span>step 3</span>
             <span>add-ons</span>
           </div>
         </div>
         <div className="headerItem__container">
-          <button className={step > 3 ? 'paint' : ''}>4</button>
+          <button className={step > 3 ? 'paint' : ''} onClick={() => setStep(4)}><span>4</span></button>
           <div className="headerInfo__container">
             <span>step 4</span>
             <span>summary</span>
